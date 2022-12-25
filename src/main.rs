daed44558d749643ab798ada36135d912374e801
Tarrence van As <tarrence@vanas.family>
2022-12-25 12:46:52 -0500

    feat: add kayak ui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, winit::WinitSettings};
+use kayak_ui::prelude::{widgets::*, *};
 
 mod common;
 mod hustlers;
@@ -9,27 +10,46 @@ pub const RESOLUTION: f32 = 16.0 / 9.0;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::hex("000000").unwrap()))
-        .add_plugins(DefaultPlugins.set(
-            // here we configure the main window
-            WindowPlugin {
-                window: WindowDescriptor {
-                    width: 375.0,
-                    height: 720.0,
-                    title: "HUSTLE.EXE".to_string(),
-                    ..Default::default()
-                },
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: 375.0,
+                height: 720.0,
+                title: "HUSTLE.EXE".to_string(),
                 ..Default::default()
             },
-        ))
+            ..Default::default()
+        }))
+        .add_plugin(KayakContextPlugin)
+        .add_plugin(KayakWidgets)
         .insert_resource(WinitSettings::desktop_app())
-        .add_startup_system(setup)
+        .add_startup_system(startup)
         .add_plugin(hustlers::HustlersPlugin)
         .add_plugin(markets::MarketsPlugin)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Camera
-    commands.spawn(Camera2dBundle::default());
+fn startup(
+    mut commands: Commands,
+    mut font_mapping: ResMut<FontMapping>,
+    asset_server: Res<AssetServer>,
+) {
+    font_mapping.set_default(asset_server.load("roboto.kayak_font"));
+
+    let mut widget_context = KayakRootContext::new();
+    widget_context.add_plugin(KayakWidgetsContextPlugin);
+    let parent_id = None;
+    rsx! {
+        <KayakAppBundle>
+            <TextWidgetBundle
+                text={TextProps {
+                    content: "Hello World".into(),
+                    size: 20.0,
+                    ..Default::default()
+                }}
+            />
+        </KayakAppBundle>
+    };
+
+    commands.spawn(UICameraBundle::new(widget_context));
 }
